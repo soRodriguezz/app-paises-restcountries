67c1073be62ca43a6a61dca48da4b44be4730039
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-05-27 15:06:13 -0400

    @input @output

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable/*, of */} from 'rxjs';
+import { Observable /*, of */ } from 'rxjs';
+import { Country } from '../interfaces/pais.interface';
 // import { catchError } from 'rxjs/operators';
 
 @Injectable({
@@ -11,11 +12,11 @@ export class PaisService {
 
   constructor(private http: HttpClient) {}
 
-  buscarPais(termino: string): Observable<any> {
-    const url = ` ${this.apiUrl}/name/${termino}`;
-    return this.http.get(url);
-    // .pipe(
-    //   catchError(err => of([]) ) // of genera observable
-    // );
+  buscarPais(termino: string): Observable<Country[]> { // oberservable que emite un arreglo de pais
+    const url = ` ${ this.apiUrl }/name/${ termino }`;
+    return this.http.get<Country[]>( url );
+    /* .pipe(
+      catchError(err => of([]) ) // of genera observable
+     ); */
   }
 }
