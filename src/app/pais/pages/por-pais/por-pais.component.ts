194f087fa5137e7f9abaab0db314cb08836f67f0
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-05-27 10:52:40 -0400

    first commit

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [],
+})
+export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+
+  constructor(private paisService: PaisService) {}
+
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino).subscribe(
+      (respuesta) => {
+        console.log(respuesta);
+      },
+      (err) => {
+        this.hayError = true;
+      }
+    );
+  }
+}
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable/*, of */} from 'rxjs';
+// import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaisService {
+  private apiUrl: string = 'https://restcountries.eu/rest/v2';
+
+  constructor(private http: HttpClient) {}
+
+  buscarPais(termino: string): Observable<any> {
+    const url = ` ${this.apiUrl}/name/${termino}`;
+    return this.http.get(url);
+    // .pipe(
+    //   catchError(err => of([]) ) // of genera observable
+    // );
+  }
+}
