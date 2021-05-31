eb75ef0d3e7d20034a605de4df006715ae7dc7b2
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-05-31 13:25:21 -0400

    data pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+//Operador de transformador, obtiene un observable y devuelve otro
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country; //Decir a typscript que puede ser null
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
+      )
+      .subscribe(pais => this.pais = pais);
 
+    /* 
+      this.activatedRoute.params.subscribe(({ id }) => {
+        console.log(id);
+        this.paisService.getPaisPorAlpha(id).subscribe((pais) => {
+          console.log(pais);
+        });
+      }); 
+    */
+  }
 }
