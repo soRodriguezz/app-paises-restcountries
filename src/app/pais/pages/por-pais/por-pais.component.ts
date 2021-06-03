9ad238f4b68019232277d91fa337242088838086
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-06-03 14:09:48 -0400

    app-pais-end

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,16 +5,25 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias:boolean = false;
 
   constructor(private paisService: PaisService) {}
 
   buscar(termino: string) {
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
 
@@ -31,8 +40,22 @@ export class PorPaisComponent {
   }
 
   sugerencias(termino: string) {
+    this.mostrarSugerencias = true;
     this.hayError = false;
-    // TODO: Crear sugerencia
+    this.termino = termino;
+
+    this.paisService
+      .buscarPais(termino)
+      .subscribe(
+        (paises) => (
+          (this.paisesSugeridos = paises.splice(0, 5)),
+          (err) => (this.paisesSugeridos = [])
+        )
+      );
   }
 
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
+   
+  }
 }
